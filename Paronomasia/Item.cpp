354ca176e47d894c64fdc9d710c145d33932b4a6
unknown <loneliersinner@gmail.com>
2021-12-19 03:54:20 +0800

    Item

--- Paronomasia/Item.cpp
+++ Paronomasia/Item.cpp
@@ -5,16 +5,8 @@ using std::endl;
 
 Item::Item(string name, int id, int locationID)
 {
-	if (!name.empty() && id >= 0 && locationID >= 0 )
-	{
 		this->name = name;
 		this->id = id;
 		this->locationID = locationID;
-	}
-	else
-	{
-		this->name = "ACE";
-		this->id = 18;
-		cout << "不能没有名字或者物品id不能小于零" << endl;
-	}
+
 }
